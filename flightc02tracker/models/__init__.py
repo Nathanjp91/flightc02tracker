674a7ceb6b43e7f266a5997e296a3d7bcc5b8a57
Nathan Perkins <nathanjp91@gmail.com>
2022-10-18 18:55:50 +0100

    models structure

---
+++ flightc02tracker/models/__init__.py
@@ -0,0 +1,12 @@
+"""
+This file is the common initialization routines for the Models Sub library
+"""
+import os
+
+import motor.motor_asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client[os.environ["DB_ENVIRONMENT"]]
